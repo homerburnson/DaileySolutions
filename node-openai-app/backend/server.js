f5131b7629cf1dca09723a778ca026d109852302
homerburnson <63058487+homerburnson@users.noreply.github.com>
2025-05-04 15:04:19 +0100

    added node version

--- node-openai-app/backend/server.js
+++ node-openai-app/backend/server.js
@@ -31,7 +31,7 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.AUTH0_SECRET,
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
     issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
 };
